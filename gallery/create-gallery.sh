f0d76a2bf693db420ec55eefe2bf5d79f691dcd1
Claudio Scheer <claudioscheer@protonmail.com>
2023-07-09 20:43:09 -0300

    feat: keep previous images from thumb folder

--- gallery/create-gallery.sh
+++ gallery/create-gallery.sh
@@ -2,13 +2,23 @@
 
 json='['
 
+# Add existing images from the "large" folder.
 for image in ./large/*; do
+  if [ -f "$image" ]; then
     image_name=$(basename ${image})
     thumb="./thumb/${image_name}"
     convert -thumbnail 300 $image $thumb
     json=$json'{"thumb": "./gallery/'${thumb}'"},'
+  fi
 done
 
+# Add existing images from the "thumb" folder.
+for thumb in ./thumb/*; do
+  thumb_name=$(basename "$thumb")
+  json=$json'{"thumb": "./gallery/thumb/'${thumb_name}'"},'
+done
+
+# Remove the last comma.
 json=${json::-1}
 json=$json']'
 echo $json > ./gallery.json
