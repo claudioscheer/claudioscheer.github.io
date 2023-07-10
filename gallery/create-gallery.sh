1f237f30842f13f6c3766562bb7b371e49154ac3
Claudio Scheer <claudioscheer@protonmail.com>
2023-07-09 21:41:50 -0300

    feat: add images from flickr

--- gallery/create-gallery.sh
+++ gallery/create-gallery.sh
@@ -2,20 +2,22 @@
 
 json='['
 
+# Add existing images from the "thumb" folder.
+for thumb in ./thumb/*; do
+  if [ -f "$thumb" ]; then
+    thumb_name=$(basename "$thumb")
+    json=$json'{"thumb": "./gallery/thumb/'${thumb_name}'"},'
+  fi
+done
+
 # Add existing images from the "large" folder.
 for image in ./large/*; do
   if [ -f "$image" ]; then
     image_name=$(basename ${image})
-    thumb="./thumb/${image_name}"
-    convert -thumbnail 300 $image $thumb
+    thumb="thumb/${image_name}"
+    convert -thumbnail 600 $image $thumb
     json=$json'{"thumb": "./gallery/'${thumb}'"},'
   fi
-done
-
-# Add existing images from the "thumb" folder.
-for thumb in ./thumb/*; do
-  thumb_name=$(basename "$thumb")
-  json=$json'{"thumb": "./gallery/thumb/'${thumb_name}'"},'
 done
 
 # Remove the last comma.
