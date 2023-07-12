262f2bad2bf173bfd40936fc5746710d26ec319e
Claudio Scheer <claudioscheer@protonmail.com>
2023-07-11 21:34:19 -0300

    fix: gallery css

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,21 @@
+const classes =
+  "lazy w-full transition-transform duration-300 ease-in-out transform hover:scale-105".split(
+    " ",
+  );
+
 fetch("/gallery/gallery.json")
   .then((response) => response.json())
   .then((gallery) => {
     gallery.map((x) => {
-      let galleryItem = document.createElement("div");
+      const galleryItem = document.createElement("div");
       galleryItem.classList.add("overflow-hidden");
 
-      let galleryItemImg = document.createElement("img");
+      const galleryItemImg = document.createElement("img");
       galleryItemImg.src = x.thumb;
-      galleryItemImg.classList.add(
-        "w-full transition-transform duration-300 ease-in-out transform hover:scale-105".split(
-          " ",
-        ),
-      );
+
+      for (const c of classes) {
+        galleryItemImg.classList.add(c);
+      }
       galleryItemImg.loading = "lazy";
 
       galleryItem.appendChild(galleryItemImg);
